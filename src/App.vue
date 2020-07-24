41b5d68b80ce34a02c309c442bebdda7804e08c2
肌肉仔 <itzhengwei163.com>
2020-07-24 14:50:38 +0800

    导入样式初始化

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <van-button type="warning">警告按钮</van-button>
     <i class="iconfont iconxingxing2"></i>
     <i class="iconfont iconbianzu4"></i>
+    <i class="iconfont iconyanzhengma"></i>
   </div>
 </template>
 
@@ -13,6 +14,9 @@ export default {}
 
 <style lang="less">
 div {
+  width: 200px;
+  height: 200px;
+  color: @warn-color;
   background-color: @success-color;
 }
 </style>
