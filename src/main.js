3869875417b59bfc687817bee46a0d50ed75e882
肌肉仔 <itzhengwei163.com>
2020-07-24 10:27:30 +0800

    下载和导入normalize.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 导入normalize.css 抹平差异
+import 'normalize.css'
 Vue.config.productionTip = false
 
 new Vue({
