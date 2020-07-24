8e9b472f72a14c9c6ed93579b288f3df22fb905b
肌肉仔 <itzhengwei163.com>
2020-07-24 12:27:48 +0800

    导入自动导入的全局less变量

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,8 @@
 export default {}
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+div {
+  background-color: @success-color;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const autoprefixer = require('autoprefixer')
 const pxtorem = require('postcss-pxtorem')
-
+const path = require('path')
 module.exports = {
   outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production' ? '/vant-demo/' : '/',
+
   css: {
     loaderOptions: {
       postcss: {
@@ -15,6 +16,13 @@ module.exports = {
           })
         ]
       }
+    }
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, './src/style/var.less')]
     }
   }
 }
